0295aba0358ac1b5222320773c74609f33579052
Benoît <benoit.guillot@akretion.com.br>
2016-01-22 14:38:10 +0100

    [REF] fix cron and ref duplicate code

--- account_invoice_subcontractor/subcontractor_work.py
+++ account_invoice_subcontractor/subcontractor_work.py
@@ -161,92 +161,126 @@ class SubcontractorWork(models.Model):
                 work.subcontractor_state = (work.supplier_invoice_line_id.
                                             invoice_id.state)
 
-    @api.model
-    def _prepare_subcontractor_invoice_data(self, work):
-        journal = self.env['account.journal'].search([
-            ('type', '=', 'sale'),
-            ('company_id', '=', work.subcontractor_company_id.id)
-        ], limit=1)
+    @api.multi
+    def check(self):
+        partner_id = self[0].customer_id.id
+        for work in self:
+            if partner_id != work.customer_id.id:
+                raise UserError(
+                    _('All the work should belong to the same supplier'))
+            elif work.supplier_invoice_line_id:
+                raise UserError(
+                    _('This work has been already invoiced!'))
+            elif work.state not in ('open', 'paid'):
+                raise UserError(
+                    _("Only works with the state 'open' "
+                      " or 'paid' can be invoiced"))
+            elif work.subcontractor_type != 'internal':
+                raise UserError(
+                    _("You can invoice on only the internal subcontractors"))
+
+    @api.multi
+    def _prepare_invoice(self):
+        self.ensure_one()
+        journal_obj = self.env['account.journal']
+        inv_obj = self.env['account.invoice']
+        company = self.subcontractor_company_id
+        journal = journal_obj.search([
+            ('company_id', '=', company.id),
+            ('type', '=', 'sale')],
+            limit=1)
         if not journal:
-            company = work.subcontractor_company_id
             raise UserError(
-                _('Please define sale journal for this company: "%s" (id:%d).')
+                    _('Please define sale journal for this company: "%s" (id:%d).')
                 % (company.name, company.id))
-        subcontractor_company = work.subcontractor_company_id
-        users = self.pool['res.users'].search([
-            ('company_id', '=', subcontractor_company.id)])
-        return {
-            'origin': self.company_id.name + _(' Invoice: ') + str(
-                work.invoice_id.number),
-            'type': 'out_invoice',
+        onchange_vals = inv_obj.onchange_partner_id(
+            'out_invoice',  self.customer_id.id)
+        invoice_vals = onchange_vals['value']
+        subcontractor_company = self.company_id
+        user = self.env['res.users'].search([
+            ('company_id', '=', company.id)], limit=1)
+        invoice_vals.update({
             'date_invoice': date.today(),
-            'account_id': (self.company_id.partner_id.
-                           property_account_receivable.id),
-            'partner_id': self.company_id.partner_id.id,
+            'type': 'out_invoice',
+            'partner_id': self.customer_id.id,
             'journal_id': journal.id,
             'invoice_line': [(6, 0, [])],
-            'currency_id': (subcontractor_company.currency_id
-                            and subcontractor_company.currency_id.id),
-            'fiscal_position': (self.company_id.partner_id.
-                                property_account_position),
-            'company_id': subcontractor_company.id,
-            'partner_bank_id': subcontractor_company.bank_ids[0].id,
-            'user_id': users[0].id
-        }
+            'currency_id': company.currency_id.id,
+            'user_id': user.id,
+        })
+        return invoice_vals
 
     @api.model
-    def _prepare_subcontractor_invoice_line_data(self, invoice_vals, work):
-        invoice_line_obj = self.pool['account.invoice.line']
-        line_data = invoice_line_obj.sudo().product_id_change(
-            product=work.invoice_line_id.product_id.id,
+    def _prepare_invoice_line(self, invoice):
+        self.ensure_one()
+        invoice_line_obj = self.env['account.invoice.line']
+        line_data = invoice_line_obj.product_id_change(
+            product=self.sudo().invoice_line_id.product_id.id,
             uom_id=False,
-            qty=work.quantity,
-            name=work.name,
+            qty=self.quantity,
+            name=self.name,
             type='out_invoice',
-            partner_id=invoice_vals.get('partner_id'),
-            fposition_id=invoice_vals.get('fiscal_position'),
-            price_unit=work.cost_price_unit,
-            currency_id=invoice_vals.get('currency_id'),
-            company_id=invoice_vals.get('company_id'))
-        line_data.update({
-            'uos_id': work.uos_id.id,
-            'invoicing_type': work.invoice_line_id.invoicing_type,
-            'discount': work.invoice_line_id.discount,
+            partner_id=invoice.partner_id and invoice.partner_id.id or False,
+            fposition_id=invoice.fiscal_position and invoice.fiscal_position.id or False,
+            price_unit=self.cost_price_unit,
+            currency_id=invoice.currency_id and invoice.currency_id.id or False,
+            company_id=invoice.company_id and invoice.company_id.id or False)
+        line_vals = line_data['value']
+        line_vals.update({
+            'uos_id': self.uos_id.id,
+            'price_unit': self.cost_price_unit,
+            'invoice_id': invoice.id,
+            'discount': self.sudo().invoice_line_id.discount,
+            'quantity': self.quantity,
+            'product_id': self.sudo().invoice_line_id.product_id.id,
             'invoice_line_tax_id': [
                 (6, 0, line_data['value']['invoice_line_tax_id'])],
-            'name': "Client final %s: %s" % (work.end_customer_id.name,
-                                             work.name),
-            'subcontractor_work_invoiced_id': work.id,
+            'subcontractor_work_invoiced_id': self.id,
+            'name': "Client final %s :%s" % (
+                self.end_customer_id.name,
+                self.name),
         })
-        return line_data
+        return line_vals
+
+    @api.multi
+    def invoice_from_work(self):
+        invoice_line_obj = self.env['account.invoice.line']
+        invoice_obj = self.env['account.invoice']
+        invoices = self.env['account.invoice']
+        current_employee_id = None
+        current_invoice_id = None
+        for work in self:
+            if (current_employee_id != work.employee_id
+                    or current_invoice_id != work.invoice_id):
+                invoice_vals = work._prepare_invoice()
+                invoice = invoice_obj.create(invoice_vals)
+                current_employee_id = work.employee_id
+                current_invoice_id = work.invoice_id
+                invoices |= invoice
+            inv_line_data = (
+                work._prepare_invoice_line(invoice))
+            # Need sudo because odoo prefetch de work.invoice_id
+            # and try to read fields on it and that makes access rules fail
+            inv_line = invoice_line_obj.sudo().create(inv_line_data)
+            invoice.sudo().write({'invoice_line': [(6, 0, [inv_line.id])]})
+        invoices.button_reset_taxes()
+        return invoices
 
     @api.multi
     def _scheduler_action_subcontractor_invoice_create(self):
-        invoice_line_obj = self.pool['account.invoice.line']
-        invoice_obj = self.pool['account.invoice']
         date_filter = date.today() - timedelta(days=7)
-        subcontractor_works = self.search([
-            ('invoice_id.date_invoice', '<=', date_filter),
-            ('subcontractor_invoice_line_id', '=', False),
-            ('subcontractor_type', '=', 'internal'),
-            ('state', 'in', ['open', 'paid'])
-        ], order=['employee_id', 'invoice_id'])
-        current_employee_id = None
-        current_invoice_id = None
-        for work in subcontractor_works:
-            if (current_employee_id != work.employee_id
-                    or current_invoice_id != work.invoice_id):
-                # create invoice, as the internal subcontrator
-                invoice_vals = self.sudo()._prepare_subcontractor_invoice_data(
-                    work)
-                invoice = invoice_obj.sudo().create(invoice_vals)
-                current_employee_id = work.employee_id
-                current_invoice_id = work.invoice_id
-            # create invoice line, as the internal subcontractor
-            inv_line_data = (
-                self.sudo()._prepare_subcontractor_invoice_line_data(
-                    invoice_vals, work))
-            inv_line_id = invoice_line_obj.sudo().create(inv_line_data)
-            # update invoice with invoice line created
-            invoice.write({'invoice_line': [(6, 0, [inv_line_id])]})
+        subcontractors = self.env['hr.employee'].search(
+            [('subcontractor_type', '=', 'internal'),
+             ('subcontractor_company_id', '!=', False)])
+        for subcontractor in subcontractors:
+            if not subcontractor.subcontractor_company_id.intercompany_user_id:
+                continue
+            subcontractor_works = self.sudo(
+                subcontractor.subcontractor_company_id.intercompany_user_id).search([
+                    ('invoice_id.date_invoice', '<=', date_filter),
+                    ('subcontractor_invoice_line_id', '=', False),
+                    ('subcontractor_type', '=', 'internal'),
+                    ('state', 'in', ['open', 'paid'])
+                ], order='employee_id, invoice_id')
+            subcontractor_works.invoice_from_work()
         return True
--- account_invoice_subcontractor/wizard/subcontractor_invoice_work.py
+++ account_invoice_subcontractor/wizard/subcontractor_invoice_work.py
@@ -30,108 +30,13 @@ class SubcontractorInvoiceWork(models.TransientModel):
     _name = 'subcontractor.invoice.work'
     _description = 'subcontractor invoice work'
 
-    @api.model
-    def _check(self, works):
-        partner_id = works[0].customer_id.id
-        for work in works:
-            if partner_id != work.customer_id.id:
-                raise UserError(
-                    _('All the work should belong to the same supplier'))
-            elif work.supplier_invoice_line_id:
-                raise UserError(
-                    _('This work has been already invoiced!'))
-            elif work.state not in ('open', 'paid'):
-                raise UserError(
-                    _("Only works with the state 'open' "
-                      " or 'paid' can be invoiced"))
-            elif work.subcontractor_type != 'internal':
-                raise UserError(
-                    _("You can invoice on only the internal subcontractors"))
-
-    @api.model
-    def _prepare_invoice(self, work):
-        journal_obj = self.env['account.journal']
-        inv_obj = self.env['account.invoice']
-        journal = journal_obj.sudo().search([
-            ('company_id', '=', work.company_id.id),
-            ('type', '=', 'sale')],
-            limit=1)
-        if not journal:
-            raise UserError(
-                    _('Please define sale journal for this company: "%s" (id:%d).')
-                % (work.company_id.name, work.company_id.id))
-        onchange_vals = inv_obj.onchange_partner_id('out_invoice',  work.customer_id.id)
-        invoice_vals = onchange_vals['value']
-        subcontractor_company = work.company_id
-        user = self.env['res.users'].search([
-            ('company_id', '=', subcontractor_company.id)], limit=1)
-        invoice_vals.update({
-            'date_invoice': date.today(),
-            'type': 'out_invoice',
-            'partner_id': work.customer_id.id,
-            'journal_id': journal.id,
-            'invoice_line': [(6, 0, [])],
-            'currency_id': work.company_id.currency_id.id,
-            'user_id': user.id,
-        })
-        return invoice_vals
-
-    @api.model
-    def _prepare_invoice_line(self, work, invoice):
-        invoice_line_obj = self.env['account.invoice.line']
-        line_data = invoice_line_obj.product_id_change(
-            product=work.invoice_line_id.product_id.id,
-            uom_id=False,
-            qty=work.quantity,
-            name=work.name,
-            type='out_invoice',
-            partner_id=invoice.partner_id and invoice.partner_id.id or False,
-            fposition_id=invoice.fiscal_position and invoice.fiscal_position.id or False,
-            price_unit=work.cost_price_unit,
-            currency_id=invoice.currency_id and invoice.currency_id.id or False,
-            company_id=invoice.company_id and invoice.company_id.id or False)
-        line_vals = line_data['value']
-        line_vals.update({
-            'uos_id': work.uos_id.id,
-            'price_unit': work.cost_price_unit,
-            'invoice_id': invoice.id,
-            'discount': work.invoice_line_id.discount,
-            'quantity': work.quantity,
-            'product_id': work.invoice_line_id.product_id.id,
-            'invoice_line_tax_id': [
-                (6, 0, line_data['value']['invoice_line_tax_id'])],
-            'subcontractor_work_invoiced_id': work.id,
-            'name': "Client final %s :%s" % (
-                work.end_customer_id.name,
-                work.name),
-        })
-        return line_vals
-
     @api.multi
     def generate_invoice(self):
-        invoice_obj = self.env['account.invoice']
-        invoice_line_obj = self.env['account.invoice.line']
-        # browse work with sudo to be able to read the invoice_line
-        work_obj = self.env['subcontractor.work'].sudo()
+        work_obj = self.env['subcontractor.work']
         work_ids = self._context.get('active_ids')
         works = work_obj.browse(work_ids)
-        self._check(works)
-        current_employee_id = None
-        current_invoice_id = None
-        invoices = self.env['account.invoice']
-        for work in works:
-            if (current_employee_id != work.employee_id
-                    or current_invoice_id != work.invoice_id):
-                invoice_vals = self._prepare_invoice(work)
-                invoice = invoice_obj.create(invoice_vals)
-                current_employee_id = work.employee_id
-                current_invoice_id = work.invoice_id
-                invoices |= invoice
-            inv_line_data = (
-                self._prepare_invoice_line(work, invoice))
-            inv_line = invoice_line_obj.create(inv_line_data)
-            invoice.write({'invoice_line': [(6, 0, [inv_line.id])]})
-        invoices.button_reset_taxes()
+        works.check()
+        invoices = works.invoice_from_work()
         return {
             'name': _('Customer Invoices'),
             'view_type': 'form',
