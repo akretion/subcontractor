1401764b7d5e618834cb5341b49c41c94eba95b3
David Beal <david.beal@akretion.com>
2017-08-19 13:27:36 +0200

    Set order to subcontractor.work

--- account_invoice_subcontractor/subcontractor_work.py
+++ account_invoice_subcontractor/subcontractor_work.py
@@ -1,24 +1,7 @@
-# -*- coding: utf-8 -*-
-###############################################################################
-#
-#   account_invoice_subcontractor for OpenERP
-#   Copyright (C) 2013-TODAY Akretion <http://www.akretion.com>.
+# coding: utf-8
 #   @author Sébastien BEAU <sebastien.beau@akretion.com>
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as
-#   published by the Free Software Foundation, either version 3 of the
-#   License, or (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Affero General Public License for more details.
-#
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openerp import models, fields, api, _
 from datetime import timedelta, date
 from openerp.exceptions import Warning as UserError
@@ -38,6 +21,7 @@ INVOICE_STATE = [
 class SubcontractorWork(models.Model):
     _name = "subcontractor.work"
     _description = "subcontractor work"
+    _order = 'id desc'
 
     @api.model
     def _get_subcontractor_type(self):
