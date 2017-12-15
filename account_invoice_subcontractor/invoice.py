a4bc569496b8896452d5d0317fa22bc2cba919e7
David Beal <david.beal@akretion.com>
2017-12-15 13:11:19 +0100

    FIX onchange in invoice

--- account_invoice_subcontractor/invoice.py
+++ account_invoice_subcontractor/invoice.py
@@ -4,7 +4,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api
-# from odoo.osv import fields as old_fields
 
 
 class AccountInvoiceLine(models.Model):
@@ -32,19 +31,11 @@ class AccountInvoiceLine(models.Model):
         string='Work Amount Invalid',
         store=True)
 
-    @api.multi
-    def product_id_change(
-            self, product, uom_id, qty=0, name='',
-            type='out_invoice', partner_id=False, fposition_id=False,
-            price_unit=False, currency_id=False, company_id=None):
-        res = super(AccountInvoiceLine, self).product_id_change(
-            product, uom_id, qty=qty, name=name,
-            type=type, partner_id=partner_id, fposition_id=fposition_id,
-            price_unit=price_unit, currency_id=currency_id,
-            company_id=company_id)
-        product = self.env['product.product'].browse(product)
-        res['value']['subcontracted'] = product.subcontracted
-        return res
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        super(AccountInvoiceLine, self)._onchange_product_id()
+        for rec in self:
+            rec.subcontracted = rec.product_id.subcontracted
 
     @api.depends('invoice_id', 'invoice_id.type')
     @api.multi
