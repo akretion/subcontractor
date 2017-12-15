b96035219f4269aeafb7fcb90c7ec4efa71d33dc
David Beal <david.beal@akretion.com>
2017-12-15 13:26:17 +0100

    MIGR wizard

--- account_invoice_subcontractor/wizard/subcontractor_invoice_work.py
+++ account_invoice_subcontractor/wizard/subcontractor_invoice_work.py
@@ -1,29 +1,10 @@
-# -*- coding: utf-8 -*-
-###############################################################################
-#
-#   account_invoice_subcontractor for OpenERP
-#   Copyright (C) 2015-TODAY Akretion <http://www.akretion.com>.
+# coding: utf-8
+# © 2015 Akretion
 #   @author Sébastien BEAU <sebastien.beau@akretion.com>
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as
-#   published by the Free Software Foundation, either version 3 of the
-#   License, or (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Affero General Public License for more details.
-#
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
-from openerp.tools.translate import _
-from openerp.exceptions import Warning as UserError
-from datetime import date
+from odoo import models, api
+from odoo.tools.translate import _
 
 
 class SubcontractorInvoiceWork(models.TransientModel):
@@ -39,7 +20,7 @@ class SubcontractorInvoiceWork(models.TransientModel):
         invoices = works.invoice_from_work()
         return {
             'name': _('Customer Invoices'),
-            'view_type': 'form',
+            # 'view_type': 'form',
             'view_mode': 'tree,form',
             'res_model': 'account.invoice',
             'context': "{'type':'out_invoice'}",
