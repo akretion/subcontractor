b24f1d1c75a10aca4c5034fdbcf454378cc4e44d
Florian da Costa <florian.dacosta@akretion.com>
2022-06-07 10:51:43 +0200

    Fix invoice view smart button when associated with another module adding smart button + fix invoice creation from sale order

--- account_invoice_subcontractor/models/sale.py
+++ account_invoice_subcontractor/models/sale.py
@@ -1,14 +1,12 @@
-# © 2015 Akretion
-# @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    def _prepare_invoice_line(self, qty):
-        vals = super()._prepare_invoice_line(qty)
+    def _prepare_invoice_line(self, **optional_values):
+        vals = super()._prepare_invoice_line(**optional_values)
         vals["subcontracted"] = self.product_id.subcontracted or False
         return vals
