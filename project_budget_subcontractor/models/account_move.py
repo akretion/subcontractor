a6adbd120158644fb6bd645354d34e17d157542e
Florian da Costa <florian.dacosta@akretion.com>
2026-06-15 16:33:42 +0200

    [FIX] project_budget_subcontractor: section and note dont need a project

--- project_budget_subcontractor/models/account_move.py
+++ project_budget_subcontractor/models/account_move.py
@@ -25,7 +25,9 @@ class AccountMove(models.Model):
     def _post(self, soft=True):
         for move in self:
             if move.use_budget and move.move_type in ["out_invoice", "out_refund"]:
-                if move.invoice_line_ids.filtered(lambda line: not line.project_id):
+                if move.invoice_line_ids.filtered(
+                    lambda line: not line.project_id and line.display_type == "product"
+                ):
                     raise UserError(
                         self.env._(
                             "You can't post a move containing lines without project "
